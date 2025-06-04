7c73a7a06fb330611e6a05c6aaaa73673db068b0
hasanahsnh <sanah.hasanah0204@gmail.com>
2025-06-05 00:08:13 +0800

    add: controller pesanan

---
+++ app/Http/Controllers/Firebase/PesananController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Firebase;
+
+use App\Http\Controllers\Controller;
+use Kreait\Firebase\Contract\Auth;
+use Kreait\Firebase\Contract\Database;
+use Kreait\Firebase\Contract\Storage;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Str;
+use Kreait\Firebase\Exception\FirebaseException;
+
+class PesananController extends Controller
+{
+    protected $database;
+    protected $refTableName;
+    protected $auth;
+    protected $storage;
+
+    public function __construct(Database $database, Auth $auth, Storage $storage) {
+        $this->database = $database;
+        $this->auth = $auth;
+        $this->refTableName = 'orders';
+        $this->storage = $storage;
+    }
+
+    function index() {
+        $dataPesanans = $this->database->getReference($this->refTableName)->getValue();
+        return response()->json([
+            'success' => true,
+            'data' => $dataPesanans
+        ]);
+    }
+}
