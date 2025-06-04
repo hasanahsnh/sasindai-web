a63fd1f0457f80d357a3f4aa186b390064200d73
hasanahsnh <sanah.hasanah0204@gmail.com>
2025-06-05 00:23:47 +0800

    fix: mitra/_navbar

--- resources/views/mitra/partials/_navbar.blade.php
+++ resources/views/mitra/partials/_navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
             <div class="navbar-brand-wrapper d-flex justify-content-center">
         <div class="navbar-brand-inner-wrapper d-flex justify-content-between align-items-center w-100">  
-          <a class="navbar-brand brand-logo" href=""><img src="{{ asset('images/cms_logo.svg') }}" alt="logo"/></a>
+          <a class="navbar-brand brand-logo" href=""><img src="{{ asset('images/cms-logo.svg') }}" alt="logo"/></a>
           <a class="navbar-brand brand-logo-mini" href=""><img src="{{ asset('pengunjung/images/sascode-logo.jpg') }}" alt="logo"/></a>
           <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
             <span class="mdi mdi-sort-variant"></span>
