122eabb93b33ec4efc4f4489b6507d522d314a24
hasanahsnh <sanah.hasanah0204@gmail.com>
2025-06-05 00:16:12 +0800

    fix: blade berita

--- resources/views/admin/pages/berita.blade.php
+++ resources/views/admin/pages/berita.blade.php
@@ -58,18 +58,13 @@
               <div class="card-body">
                 <div class="d-flex align-items-center mb-3">
                   <h1 class="card-title" style="font-size:16px; color:black; display: inline-block; border-bottom: 2px solid #522258; padding-bottom: 10px;">
-                    DATA BERITA
+                    Rilis media
                   </h1>
                   <div class="d-none d-md-flex ms-3" style="margin-left: 60px;">
                     <a type="button" data-toggle="modal" data-target="#modalTambah" onclick="setRedirectUrl('{{ route('tambah-berita') }}')"
                     style="font-size:14px; margin-right: 30px; color:#8D0B41; display: flex; align-items: center;">
                       <i class="mdi mdi-plus-box" style="font-size: 20px; vertical-align: middle; margin-right: 5px; color:#8D0B41"></i>
                         TAMBAH BERITA
-                    </a>
-                    <a href="{{ route('download-data-berita') }}" type="button"
-                    style="font-size:14px; color:#757575; display: flex; align-items: center; text-decoration: none;">
-                      <i class="mdi mdi-download text-muted" style="font-size: 20px; vertical-align: middle; margin-right: 5px;"></i>
-                        UNDUH DATA
                     </a>
                   </div>
 
@@ -84,10 +79,6 @@
                               <i class="mdi mdi-plus-box" style="font-size: 20px; vertical-align: middle; margin-right: 5px; color:#8D0B41"></i>
                               TAMBAH BERITA
                           </a>
-                          <a href="{{ route('download-data-berita') }}" class="dropdown-item" type="button" style="font-size:14px; color:#757575; display: flex; align-items: center; text-decoration: none;">
-                              <i class="mdi mdi-download text-muted" style="font-size: 20px; vertical-align: middle; margin-right: 5px;"></i>
-                              UNDUH DATA
-                          </a>
                       </div>
                     </div>
                   </div>
@@ -97,12 +88,13 @@
                   <table class="table table-hover w-100">
                     <thead>
                       <tr>
-                        <th>Lihat</th>
-                        <th>Judul Artikel</th>
-                        <th>Jurnalis</th>
-                        <th>Konten Berita</th>
+                        <th>Judul</th>
+                        <th>Penulis</th>
+                        <th>Konten</th>
                         <th>Tanggal Terbit</th>
                         <th>Foto Berita</th>
+                        <th>Dibuat pada</th>
+                        <th>Diperbarui pada</th>
                         <th>Action</th>
                       </tr>
                     </thead>
@@ -112,9 +104,6 @@
                         @foreach ($beritas as $key => $item)
                         @if(!empty($item['judulArtikel']) || !empty($item['jurnalis']) || !empty($item['kontenBerita']) || !empty($item['tanggalTerbit']) || !empty($item['fotoBeritaUrl']))
                           <tr>
-                            <td>
-                              <a href="" data-toggle="modal" data-target="#modalLihat{{ $key }}" title="Detail"><i class="mdi mdi-eye" style="margin-right: 15px;"></i></a>    
-                            </td>
                             <td>{{ $item['judulArtikel'] }}</td>
                             <td>{{ $item['jurnalis'] }}</td>
                             <td>{{ $item['kontenBerita'] }}</td>
@@ -122,6 +111,8 @@
                             <td>
                               <a href="{{ $item['fotoBeritaUrl'] }}" target="_blank" title="Unduh foto berita">{{ $item['fotoBeritaUrl'] }}</a>
                             </td>
+                            <td>{{ $item['createAt']  ?? 'Tanggal tidak ditemukan' }}</td>
+                            <td>{{ $item['updateAt']  ?? 'Tanggal tidak ditemukan' }}</td>
                             <td>
                               <a href="" data-toggle="modal" data-target="#modalEdit{{ $key }}" title="Edit"><i class="fas fa-edit" style="margin-right: 15px;"></i></a>    
                               <a href="" data-toggle="modal" data-target="#modalHapus{{ $key }}" title="Hapus"><i class="fa-solid fa-trash" style="color: red"></i></a>     
@@ -148,31 +139,25 @@
                                           <div class="form-group row">
                                             <label for="exampleInputUsername2" class="col-sm-3 col-form-label">Judul Artikel</label>
                                             <div class="col-sm-9">
-                                              <input type="text" style="border: 2px solid #8D0B41; border-radius: 4px;" class="form-control" value="{{ $item['judulArtikel'] }}" id="judul_artikel" name="judul_artikel" placeholder="Judul artikel" required>
+                                              <input type="text" style="border: 1px solid #8D0B41; border-radius: 4px;" class="form-control" value="{{ $item['judulArtikel'] }}" id="judul_artikel" name="judul_artikel" placeholder="Judul artikel" required>
                                             </div>
                                           </div>
                                           <div class="form-group row">
                                             <label for="exampleInputUsername2" class="col-sm-3 col-form-label">Jurnalis</label>
                                             <div class="col-sm-9">
-                                              <input type="text" style="border: 2px solid #8D0B41; border-radius: 4px;" class="form-control" value="{{ $item['jurnalis'] }}" id="jurnalis" name="jurnalis" placeholder="Jurnalis" required>
+                                              <input type="text" style="border: 1px solid #8D0B41; border-radius: 4px;" class="form-control" value="{{ $item['jurnalis'] }}" id="jurnalis" name="jurnalis" placeholder="Jurnalis" required>
                                             </div>
                                           </div>
                                           <div class="form-group row">
                                             <label for="exampleInputEmail2" class="col-sm-3 col-form-label">Konten Berita</label>
                                             <div class="col-sm-9">
-                                              <textarea class="form-control" style="border: 2px solid #8D0B41; border-radius: 4px;" id="konten_berita" name="konten_berita" placeholder="Konten berita" required>{{ $item['kontenBerita'] }}</textarea>
-                                            </div>
-                                          </div>
-                                          <div class="form-group row">
-                                            <label for="exampleInputUsername2" class="col-sm-3 col-form-label">Tanggal Terbit</label>
-                                            <div class="col-sm-9">
-                                              <input type="date" style="border: 2px solid #8D0B41; border-radius: 4px;" class="form-control" value="{{ $item['tanggalTerbit'] }}" id="tanggal_terbit" name="tanggal_terbit" placeholder="Masukkan tanggal terbit" required>
+                                              <textarea class="form-control" style="border: 1px solid #8D0B41; border-radius: 4px;" id="konten_berita" name="konten_berita" placeholder="Konten berita" required>{{ $item['kontenBerita'] }}</textarea>
                                             </div>
                                           </div>
                                           <div class="form-group row">
                                             <label for="exampleInputMobile" class="col-sm-3 col-form-label">Foto Berita</label>
                                             <div class="col-sm-9">
-                                              <input type="file" style="border: 2px solid #8D0B41; border-radius: 4px;" class="form-control" value="{{ $item['fotoBeritaUrl'] }}" id="foto_berita" name="foto_berita" placeholder="Pilih foto">
+                                              <input type="file" style="border: 1px solid #8D0B41; border-radius: 4px;" class="form-control" value="{{ $item['fotoBeritaUrl'] }}" id="foto_berita" name="foto_berita" placeholder="Pilih foto">
                                             </div>
                                           </div>
                                           <button type="submit" class="btn btn-primary mr-2">Update</button>
@@ -198,10 +183,10 @@
                                           <span aria-hidden="true"><span class="icon-close2"></span></span>
                                         </a>
                                       <div class="card-body">
-                                        <h4 class="card-title">Hapus Motif</h4>
+                                        <h4 class="card-title">Hapus Rilis Media</h4>
                                         <p class="card-description">
                                           <i class="fas fa-exclamation-triangle" style="color: red;"></i>
-                                          Anda yakin ingin menghapus data motif yang dipilih?
+                                          Anda yakin ingin menghapus data rilis media yang dipilih?
                                         </p>
                                         <form class="forms-sample" id="formHapus{{ $key }}" action="{{ route('destroy-berita', ['id' => $key]) }}" method="POST">
                                           @csrf
@@ -216,66 +201,11 @@
                             </div>
                           </div>
 
-                          <!-- Modal Lihat -->
-                          <div class="modal fade" id="modalLihat{{ $key }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                            <div class="modal-dialog modal-dialog-centered" role="document" style="max-width: 700px; width: 100%;">
-                              <div class="modal-content rounded-0">
-                                <div class="modal-body p-4 px-5">
-                        
-                                  
-                                  <div class="main-content text-center">
-                                      
-                                      <a href="#" class="close-btn" data-dismiss="modal" aria-label="Close">
-                                          <span aria-hidden="true"><span class="icon-close2"></span></span>
-                                        </a>
-                                      <div class="card-body">
-                                        <h4 class="card-title">Berita {{ $item['judulArtikel'] }}</h4>
-                                        <form class="forms-sample" id="formEdit{{ $key }}" action="" method="">
-                                          @csrf
-                                          <div class="form-group row">
-                                            <label for="exampleInputUsername2" class="col-sm-3 col-form-label">Judul Artikel</label>
-                                            <div class="col-sm-9">
-                                              <input type="text" class="form-control" value="{{ $item['judulArtikel'] }}" id="judul_artikel" name="judul_artikel" placeholder="Judul artikel" readonly>
-                                            </div>
-                                          </div>
-                                          <div class="form-group row">
-                                            <label for="exampleInputUsername2" class="col-sm-3 col-form-label">Jurnalis</label>
-                                            <div class="col-sm-9">
-                                              <input type="text" class="form-control" value="{{ $item['jurnalis'] }}" id="jurnalis" name="jurnalis" placeholder="Jurnalis" readonly>
-                                            </div>
-                                          </div>
-                                          <div class="form-group row">
-                                            <label for="exampleInputEmail2" class="col-sm-3 col-form-label">Konten Berita</label>
-                                            <div class="col-sm-9">
-                                              <textarea class="form-control" id="konten_berita" name="konten_berita" placeholder="Konten berita" readonly>{{ $item['kontenBerita'] }}</textarea>
-                                            </div>
-                                          </div>
-                                          <div class="form-group row">
-                                            <label for="exampleInputUsername2" class="col-sm-3 col-form-label">Tanggal Terbit</label>
-                                            <div class="col-sm-9">
-                                              <input type="date" class="form-control" value="{{ $item['tanggalTerbit'] }}" id="tanggal_terbit" name="tanggal_terbit" placeholder="Masukkan tanggal terbit" readonly>
-                                            </div>
-                                          </div>
-                                          <div class="form-group row">
-                                            <label for="exampleInputMobile" class="col-sm-3 col-form-label">Foto Berita</label>
-                                            <div class="col-sm-9">
-                                              <img src="{{ $item['fotoBeritaUrl'] }}" alt="Preview Foto Berita" style="max-width: 100%; height: auto; margin-bottom: 10px;">
-                                            </div>
-                                          </div>
-                                          <button class="btn btn-light" data-dismiss="modal">Cancel</button>
-                                        </form>
-                                      </div>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-
                         @endif
                         @endforeach
                       @else
                         <tr>
-                          <td colspan="5" style="text-align: center">Data berita tidak ditemukan</td>
+                          <td colspan="9" style="text-align: center">Rilis media tidak ditemukan</td>
                         </tr>
                       @endif
                     </tbody>
@@ -298,33 +228,27 @@
                               <form class="forms-sample" id="formTambah" action="{{ url('simpan-berita') }}" method="POST" enctype="multipart/form-data">
                                 @csrf
                                 <div class="form-group row">
-                                  <label for="exampleInputUsername2" class="col-sm-3 col-form-label">Judul Artikel</label>
+                                  <label for="exampleInputUsername2" class="col-sm-3 col-form-label">Judul</label>
                                   <div class="col-sm-9">
-                                    <input type="text" style="border: 2px solid #8D0B41; border-radius: 4px;" class="form-control" id="judul_artikel" name="judul_artikel" placeholder="Judul artikel" required>
+                                    <input type="text" style="border: 1px solid #8D0B41; border-radius: 4px;" class="form-control" id="judul_artikel" name="judul_artikel" placeholder="Judul artikel" required>
                                   </div>
                                 </div>
                                 <div class="form-group row">
-                                  <label for="exampleInputEmail2" class="col-sm-3 col-form-label">Jurnalis</label>
+                                  <label for="exampleInputEmail2" class="col-sm-3 col-form-label">Penulis</label>
                                   <div class="col-sm-9">
-                                    <input type="text" style="border: 2px solid #8D0B41; border-radius: 4px;" class="form-control" id="jurnalis" name="jurnalis" placeholder="Jurnalis" required>
+                                    <input type="text" style="border: 1px solid #8D0B41; border-radius: 4px;" class="form-control" id="jurnalis" name="jurnalis" placeholder="Jurnalis" required>
                                   </div>
                                 </div>
                                 <div class="form-group row">
-                                  <label for="exampleInputEmail2" class="col-sm-3 col-form-label">Konten Berita</label>
+                                  <label for="exampleInputEmail2" class="col-sm-3 col-form-label">Konten</label>
                                   <div class="col-sm-9">
-                                    <textarea class="form-control" style="border: 2px solid #8D0B41; border-radius: 4px;" id="konten_berita" name="konten_berita" rows="4" placeholder="Konten berita" required></textarea>
+                                    <textarea class="form-control" style="border: 1px solid #8D0B41; border-radius: 4px;" id="konten_berita" name="konten_berita" rows="4" placeholder="Konten berita" required></textarea>
                                   </div>
                                 </div>
                                 <div class="form-group row">
-                                  <label for="exampleInputUsername2" class="col-sm-3 col-form-label">Tanggal Terbit</label>
+                                  <label for="exampleInputMobile" class="col-sm-3 col-form-label">Foto</label>
                                   <div class="col-sm-9">
-                                    <input type="date" style="border: 2px solid #8D0B41; border-radius: 4px;" class="form-control" id="tanggal_terbit" name="tanggal_terbit" placeholder="Masukkan tanggal terbit" required>
-                                  </div>
-                                </div>
-                                <div class="form-group row">
-                                  <label for="exampleInputMobile" class="col-sm-3 col-form-label">Foto Berita</label>
-                                  <div class="col-sm-9">
-                                    <input type="file" style="border: 2px solid #8D0B41; border-radius: 4px;" class="form-control" id="foto_berita" name="foto_berita" placeholder="Pilih Foto" required>
+                                    <input type="file" style="border: 1px solid #8D0B41; border-radius: 4px;" class="form-control" id="foto_berita" name="foto_berita" placeholder="Pilih Foto" required>
                                   </div>
                                 </div>
                                 <button type="submit" class="btn btn-primary mr-2">Submit</button>
