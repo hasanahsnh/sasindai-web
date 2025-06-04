2e23edb9b7ba75753bde4dc927fd359a882fc15c
hasanahsnh <sanah.hasanah0204@gmail.com>
2025-06-05 00:24:54 +0800

    fix: api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\Midtrans\CallbackController;
 use App\Http\Controllers\Midtrans\CheckoutController;
+use App\Http\Controllers\Midtrans\PlCallbackController;
+use App\Http\Controllers\Midtrans\PlCheckoutController;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Rajaongkir\EkspedisiController;
@@ -10,3 +12,6 @@ Route::get('/ekspedisi', [EkspedisiController::class, 'cekTarif']);
 Route::get('/rincian-kodepos', [EkspedisiController::class, 'rincianKodepos']);
 Route::post('/checkout', [CheckoutController::class, 'checkout']);
 Route::post('/midtrans/callback', [CallbackController::class, 'handleCallback']);
+
+Route::post('/cpl_checkout', [PlCheckoutController::class, 'checkout']);
+Route::post('/midtrans/cpl_callback', [PlCallbackController::class, 'handleCallback']);
